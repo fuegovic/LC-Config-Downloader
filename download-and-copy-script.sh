060f77953311ae62697b30172cea6cce8fa3376e
Fuegovic <32828263+fuegovic@users.noreply.github.com>
2024-01-03 23:41:25 -0500

    🤸

--- download-and-copy-script.sh
+++ download-and-copy-script.sh
@@ -6,7 +6,7 @@ curl -o /shared/librechat.yaml $FILE_URL
 # Check if the file "librechat.yaml" exists in the "/shared" directory before attempting to copy it
 if [ -f "/shared/librechat.yaml" ]; then
   # Copy the file to the /app
-  cp /shared/librechat.yaml ../librechat.yaml
+  cp /shared/librechat.yaml /app/librechat.yaml
   echo "File copied successfully"
 else
   echo "Error: The file librechat.yaml does not exist in the shared directory"
