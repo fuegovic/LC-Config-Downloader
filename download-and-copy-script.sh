39854705709aaa85f83a70ae18a62185650c5265
fuegovic <32828263+fuegovic@users.noreply.github.com>
2024-01-03 21:19:50 -0500

    🤸

--- download-and-copy-script.sh
+++ download-and-copy-script.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-# Use the environment variable to download the file
-curl -o /path/in/container/file.ext $FILE_URL
-
-# Copy the file to the specified container directory
-# Replace "LibreChat" with the actual container ID or name of the receiving container
-docker cp /path/in/container/file.ext $(docker-compose ps -q $TARGET_CONTAINER):/app
+curl -o /shared/librechat.yaml $FILE_URL
