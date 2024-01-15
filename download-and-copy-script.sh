3d656d3be84b9aef97656f52fd62c77950f58524
Fuegovic <32828263+fuegovic@users.noreply.github.com>
2024-01-15 15:17:00 -0500

    Update download-and-copy-script.sh
    add default value to "FILE_URL"

--- download-and-copy-script.sh
+++ download-and-copy-script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
+# Assign a default value to FILE_URL if it is unset or null
+FILE_URL=$ {FILE_URL:-https://raw.githubusercontent.com/danny-avila/LibreChat/main/librechat.example.yaml}
 curl -o /shared/librechat.yaml $FILE_URL
-
 if [ -f "/shared/librechat.yaml" ]; then
   echo "File downloaded successfully to shared volume"
 else
